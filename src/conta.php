1803b74de376eee721dd7a5749f2af04812c8dea
Thiago Greiner Rodrigues <thiagogreiner0@gmail.com>
2025-10-05 17:34:25 -0300

    Adiciona metodo contrutor e conexão com BD

--- src/conta.php
+++ src/conta.php
@@ -6,7 +6,13 @@ Class Produto {
     
     private $conexao;
 
-    
+    public function __construct
+    (
+
+    )
+    {
+        $this->conexao = new PDO('"mysql:host=localhost;dbname=banco", "root", "senha"');
+    }
 }
 
 
