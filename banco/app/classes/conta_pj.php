f94ed30dba8eb33e7d5afb918c0e3d38996f646c
Thiago Greiner Rodrigues <thiagogreiner0@gmail.com>
2025-10-12 17:50:29 -0300

    Criando atributos e método construtor

--- banco/app/classes/conta_pj.php
+++ banco/app/classes/conta_pj.php
@@ -4,5 +4,25 @@ declare(strict_types=1);
 require_once __DIR__ . '/conta.php';
 
 class ContaPJ extends Conta {
-   
+    private string $razaoSocial;
+    private string $cnpj;
+
+    // Não obrigatório
+    private ?string $nomeFantasia;
+
+    public function __construct
+    (
+        string $razaoSocial,
+        string $cnpj,
+        ?string $nomeFantasia,
+        string $telefone
+    )
+    {
+        parent::__construct($telefone);
+        $this->razaoSocial = $razaoSocial;
+        $this->cnpj = $cnpj;
+        $this->nomeFantasia = $nomeFantasia;
+    }
+
+    
 }
