9a34d05523d3f4dafd2e34ccce13d54fd6f27c8b
Thiago Greiner Rodrigues <thiagogreiner0@gmail.com>
2025-10-11 22:13:08 -0300

    Adiciona get e conexão com BD

--- banco/app/conexao.php
+++ banco/app/conexao.php
@@ -2,7 +2,20 @@
 
     class Conexao {
 
-       
-    }
+        public static function getConexao(): PDO {
+            try {
+                $dsn = "mysql:host=localhost;dbname=bank";
+                $usuario = getenv('DB_USER') ?: 'root';
+                $senha = getenv('DB_PASS') ?: '';
 
+                return new PDO($dsn, $usuario, $senha);
+
+            } catch (Exception $e) {
+                echo $e->getMessage();
+                die();
+                
+            }
+        }
+
+    }
 ?>
