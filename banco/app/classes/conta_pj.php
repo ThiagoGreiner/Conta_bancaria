9ae74be8121dc9b3f945d815b867ec58c1362399
Thiago Greiner Rodrigues <thiagogreiner0@gmail.com>
2025-10-12 17:57:22 -0300

    Criando método criar conta

--- banco/app/classes/conta_pj.php
+++ banco/app/classes/conta_pj.php
@@ -24,5 +24,38 @@ class ContaPJ extends Conta {
         $this->nomeFantasia = $nomeFantasia;
     }
 
-    
+    public function criarConta(): bool {
+        try {
+            // Iniciando transação BD
+            $this->conexao->beginTransaction();
+
+            // Cria e conta base e recupera id
+            $contaId = $this->criarContaBase($this->razaoSocial, 'PJ', $this->telefone);
+
+            // Insere dados na pessoa_juridica
+            $sqlPJ = "INSERT INTO pessoa_juridica (conta_id, razao_social, cnpj, nome_fantasia)
+                      VALUES (:conta_id, :razao_social, :cnpj, :nome_fantasia)";
+
+            $preparePJ = $this->conexao->prepare($sqlPJ);
+            $preparePJ->bindParam(':conta_id', $contaId, PDO::PARAM_INT);
+            $preparePJ->bindParam(':razao_social', $this->razaoSocial);
+            $preparePJ->bindParam(':cnpj', $this->cnpj);
+            $preparePJ->bindParam(':nome_fantasia', $this->nomeFantasia);
+
+            // Retorna bool
+            $resultado = $preparePJ->execute();
+
+            // Finaliza transação BD
+            $this->conexao->commit();
+
+            return $resultado;
+
+        } catch (PDOException $e) {
+            // Cancela transação BD
+            $this->conexao->rollBack();
+            
+            echo "Erro ao criar conta PJ: " . $e->getMessage();
+            return false;
+        }
+    }
 }
