e6da04f48ca2023f6d277eeec67f230e2589cd7d
Thiago Greiner Rodrigues <thiagogreiner0@gmail.com>
2025-10-12 17:43:09 -0300

    Adicionando __DIR__ no require_once

--- banco/app/classes/conta_pf.php
+++ banco/app/classes/conta_pf.php
@@ -1,7 +1,7 @@
 <?php
 
 declare(strict_types=1);
-require_once './banco/app/classes/Conta.php';
+require_once __DIR__ . '/conta.php';
 
 class ContaPF extends Conta {
     private string $nomeCompleto;
--- banco/app/controllers/criar_conta_pf.php
+++ banco/app/controllers/criar_conta_pf.php
@@ -1,7 +1,7 @@
 <?php
 
 declare(strict_types=1);
-require_once './banco/app/classes/conta_pf.php';
+require_once __DIR__ . '/../classes/conta_pf.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
