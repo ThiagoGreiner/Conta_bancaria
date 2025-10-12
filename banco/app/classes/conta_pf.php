3bd58ad7e4b6abc7f58421b0da2ace6705b625c2
Thiago Greiner Rodrigues <thiagogreiner0@gmail.com>
2025-10-12 15:43:09 -0300

    Adiciona query criar conta PF

--- banco/app/classes/conta_pf.php
+++ banco/app/classes/conta_pf.php
@@ -30,7 +30,31 @@ class ContaPF extends Conta {
             // Iniciando transação BD
             $this->conexao->beginTransaction();
 
+            // Recuperando id gerado na conta
+            $contaId = $this->criarContaBase($this->nomeCompleto, 'PF', $this->telefone);
+            
+            // Inseri dados na pessoa fisica
+            $sqlPF = "INSERT INTO pessoa_fisica (conta_id, nome_completo, cpf, data_nascimento) VALUES (:conta_id, :nome, :cpf, :data_nascimento)";
+            
+            $preparePF = $this->conexao->prepare($sqlPF);
+            
+            $preparePF->bindParam(':conta_id', $contaId, PDO::PARAM_INT);
+            $preparePF->bindParam(':nome', $this->nomeCompleto);
+            $preparePF->bindParam(':cpf', $this->cpf);
+            $preparePF->bindParam(':data_nascimento', $this->dataNascimento);
+
+            // Retorna bool
+            $resultado = $preparePF->execute();
+
+            // Finaliza transação BD
+            $this->conexao->commit();
+
+            return $resultado;
+
         } catch (PDOException $e) {
+            // Cancela transação BD
+            $this->conexao->rollBack();
+            
             echo "Erro ao criar conta PF: " . $e->getMessage();
             return false;
         }
