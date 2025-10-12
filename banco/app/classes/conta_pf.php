65a897d8cfce6ec8aa4141cf25cf9c4f634cfaba
Thiago Greiner Rodrigues <thiagogreiner0@gmail.com>
2025-10-12 13:58:35 -0300

    Adiciona atributos e método construtor

--- banco/app/classes/conta_pf.php
+++ banco/app/classes/conta_pf.php
@@ -5,5 +5,24 @@ require_once './banco/app/classes/Conta.php';
 
 class ContaPF extends Conta
 {
-    
+    private string $nomeCompleto;
+    private string $cpf;
+    private string $dataNascimento;
+
+    public function __construct
+    (
+        string $nomeCompleto,
+        string $cpf,
+        string $dataNascimento,
+        string $telefone
+    )
+    {
+        // Chamando o construtor da classe Conta
+        parent::__construct($telefone);
+
+        $this->nomeCompleto = $nomeCompleto;
+        $this->cpf = $cpf;
+        $this->dataNascimento = $dataNascimento;
+    }
+
 }
