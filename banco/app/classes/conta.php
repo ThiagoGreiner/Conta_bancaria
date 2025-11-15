39b87b569180a33a05b5a43e1b83ec7490352415
Thiago Greiner Rodrigues <thiagogreiner0@gmail.com>
2025-11-15 17:13:03 -0300

    Alterando para receber cpf e cnpj

--- banco/app/classes/conta.php
+++ banco/app/classes/conta.php
@@ -25,19 +25,20 @@ Class Conta {
 
         foreach ($this->conexao->query($sql) as $key => $value) {
             
-            echo 'Id: ' . $value['id'] . '<br> Titular: ' . $value['nome_titular'] . '<br> Tipo: ' . $value['tipo'] . '<br> Saldo: ' . $value['saldo'] . '<br> Status: ' . $value['status'] . '<br> Data de Abertura: ' . $value['data_abertura'] . '<br> Data de Encerramento: ' . $value['data_encerramento'] . '<br> Telefone: ' . $value['telefone'] . '<hr>';
+            echo 'Id: ' . $value['id'] . '<br> Titular: ' . $value['nome_titular'] .'<br> Documento: ' . $value['documento'] . '<br> Tipo: ' . $value['tipo'] . '<br> Saldo: ' . $value['saldo'] . '<br> Status: ' . $value['status'] . '<br> Data de Abertura: ' . $value['data_abertura'] . '<br> Data de Encerramento: ' . $value['data_encerramento'] . '<br> Telefone: ' . $value['telefone'] . '<hr>';
         }
     }
     
     // Método criar conta, base para PF e PJ
-    protected function criarContaBase(string $nomeTitular, string $tipo, string $telefone, string $senha): int {
+    protected function criarContaBase(string $nomeTitular, string $documento, string $tipo, string $telefone, string $senha): int {
 
-        $sql = "INSERT INTO conta (nome_titular, tipo, saldo, telefone, senha)
-                VALUES (:nome_titular, :tipo, 0.00, :telefone, :senha)";
+        $sql = "INSERT INTO conta (nome_titular, documento, tipo, saldo, telefone, senha)
+                VALUES (:nome_titular, :documento, :tipo, 0.00, :telefone, :senha)";
 
         $prepare = $this->conexao->prepare($sql);
 
         $prepare->bindValue(':nome_titular', $nomeTitular);
+        $prepare->bindValue(':documento', $documento);
         $prepare->bindValue(':tipo', $tipo);
         $prepare->bindValue(':telefone', $telefone);
         $prepare->bindValue(':senha', $senha);
