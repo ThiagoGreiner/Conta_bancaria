8886914752e27ca834017f7b4ef7c3e684613a94
Thiago Greiner Rodrigues <thiagogreiner0@gmail.com>
2025-11-16 09:41:25 -0300

    Adiciona painel do bancario

---
+++ banco/app/bancario/painel.php
@@ -0,0 +1,51 @@
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Painel Administrativo</title>
+</head>
+<body>
+    
+    <a href="../../public/formulario_cadastro_pj.html">
+        <button type="button">Abrir Conta PJ</button>
+    </a>
+
+    <a href="../../public/formulario_cadastro_pf.html">
+        <button type="button">Abrir Conta PF</button>
+    </a>
+
+    <hr>
+
+    <table border="1" cellspacing="15" cellpadding="5">
+        <thead>
+            <tr>
+                <th>Titular</th>
+                <th>CPF/CNPJ</th>
+                <th>Fone</th>
+                <th>Tipo de Conta</th>
+                <th>Status</th>
+                <th>Data Abertura</th>
+                <th>Data Encerramento</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th>Titular</th>
+                <th>CPF/CNPJ</th>
+                <th>Fone</th>
+                <th>Tipo de Conta</th>
+                <th>Status</th>
+                <th>Data Abertura</th>
+                <th>Data Encerramento</th>
+                <th><a href="">Alterar Senha</a></th>
+                <th><a href="">Encerrar Conta</a></th>
+                <th><a href="">Mais Opções</a></th>
+            </tr>
+        </tbody>
+    </table> 
+
+</body>
+</html>
