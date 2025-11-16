afa643095b832d36ccee50b851af4e238b85a862
Thiago Greiner Rodrigues <thiagogreiner0@gmail.com>
2025-11-16 09:56:54 -0300

    Adicionando funcionalidades PHP

--- banco/app/bancario/painel.php
+++ banco/app/bancario/painel.php
@@ -1,4 +1,15 @@
+<?php 
 
+require_once __DIR__ . '/../classes/conexao.php';
+
+// Conectando BD
+$pdo = Conexao::conectar();
+
+// Buscando contas
+$sql = "SELECT * FROM conta ORDER BY id DESC";
+$clientes = $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+
+?>
 
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -29,21 +40,25 @@
                 <th>Status</th>
                 <th>Data Abertura</th>
                 <th>Data Encerramento</th>
+                <th colspan="3">Ações</th>
             </tr>
         </thead>
         <tbody>
-            <tr>
-                <th>Titular</th>
-                <th>CPF/CNPJ</th>
-                <th>Fone</th>
-                <th>Tipo de Conta</th>
-                <th>Status</th>
-                <th>Data Abertura</th>
-                <th>Data Encerramento</th>
-                <th><a href="">Alterar Senha</a></th>
-                <th><a href="">Encerrar Conta</a></th>
-                <th><a href="">Mais Opções</a></th>
-            </tr>
+            <?php foreach ($clientes as $c): ?>
+                <tr>
+                    <td><?= $c['nome_titular'] ?></td>
+                    <td><?= $c['documento'] ?></td>
+                    <td><?= $c['telefone'] ?></td>
+                    <td><?= $c['tipo'] ?></td>
+                    <td><?= $c['status'] ?></td>
+                    <td><?= $c['data_abertura'] ?></td>
+                    <td><?= $c['data_encerramento'] ?></td>
+
+                    <td><a href="">Alterar Senha</a></td>
+                    <td><a href="">Encerrar Conta</a></td>
+                    <td><a href="">Mais Opções</a></td>
+                </tr>
+            <?php endforeach; ?>
         </tbody>
     </table> 
 
